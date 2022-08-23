ad0e7b9184fe33b0ef9102dd7aedd48f78d94bc2
Sebastian Mosquera <sebimosquera@gmail.com>
2022-08-22 23:44:19 -0300

    fix

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const reqDate = require('./middleware/date')
 const router = require('./router')
 const app = express()
 const port = 3000
 
 app.use('/', router);
-
+app.use(reqDate.date)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
